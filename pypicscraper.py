96dcdc326a175ff6e74720e2ab132a84879bb1f6
glptrst <giulio.pietroiusti@gmail.com>
2020-09-21 00:41:11 +0200

    Add try block.

--- pypicscraper.py
+++ pypicscraper.py
@@ -2,21 +2,19 @@
 
 import urllib.request
 import re
-#from bs4 import BeautifulSoup
 
-# get input (url) from user
 user_input = input()
 
-print(user_input)
-
-# check input
-
-# make http request to get page content
-with urllib.request.urlopen(user_input) as response:
-    html = response.read().decode('utf-8')
-
-# extract the link we want
-regex = re.compile(r'og:image.+content=".+').search(html).group()
-regex = re.compile(r'http.+\"').search(regex).group()
-
-print(regex[:len(regex)-1])
+while True:
+    try: 
+        # make http request to get page content
+        with urllib.request.urlopen(user_input) as response:
+            html = response.read().decode('utf-8')
+        # extract the link we want
+        regex = re.compile(r'og:image.+content=".+').search(html).group()
+        regex = re.compile(r'http.+\"').search(regex).group()
+        print(regex[:len(regex)-1])
+        break;
+    except:
+        user_input = input("Something's gone wrong...try again:\n")
+        
